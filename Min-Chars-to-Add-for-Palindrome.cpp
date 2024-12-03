0ffa3bf113bd9be3caed45405c7425c201895af3
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-12-03 20:37:24 +0530

    Create Min-Chars-to-Add-for-Palindrome.cpp

---
+++ Min-Chars-to-Add-for-Palindrome.cpp
@@ -0,0 +1,71 @@
+##############################################################################  Question  ######################################################################################################
+
+Given a string s, the task is to find the minimum characters to be added at the front to make the string palindrome.
+
+Note: A palindrome string is a sequence of characters that reads the same forward and backward.
+
+Examples:
+
+Input: s = "abc"
+Output: 2
+Explanation: Add 'b' and 'c' at front of above string to make it palindrome : "cbabc"
+Input: s = "aacecaaaa"
+Output: 2
+Explanation: Add 2 a's at front of above string to make it palindrome : "aaaacecaaaa"
+Constraints:
+1 <= s.size() <= 106
+
+
+##############################################################################  Solution  ######################################################################################################//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minChar(string& s) {
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        s+="$";
+        s+=rev;
+        int pre=0,suff=1;
+        vector<int>LPS(s.length(),0);
+        while(suff<s.length()){
+            if(s[suff]==s[pre]){
+                LPS[suff]=pre+1;
+                suff++;
+                pre++;
+            }
+          else{
+              if(pre==0)
+              suff++;
+             else
+             pre=LPS[pre-1];
+          }  
+        }
+        return rev.length()-LPS[s.length()-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.minChar(str);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
